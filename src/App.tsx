dfd2bb5ff261d5ac99bb9a7b70a821150982f2db
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-10-04 22:38:07 +0900

    ポケモンAPIの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  axios
+    .get("https://pokeapi.co/api/v2/pokemon/")
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch(() => {
+      console.log("Error");
+    });
   return <></>;
 }
 
